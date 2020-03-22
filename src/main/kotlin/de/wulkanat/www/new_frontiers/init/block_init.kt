edbe09e8078151e7680a312aeea1bae4d1be3189
Thea Schöbl <dev@theaninova.de>
2020-03-22 21:48:45 +0100

    Resolve Cyclic Dependency

--- src/main/kotlin/de/wulkanat/www/new_frontiers/init/block_init.kt
+++ src/main/kotlin/de/wulkanat/www/new_frontiers/init/block_init.kt
@@ -4,29 +4,7 @@ import de.wulkanat.www.new_frontiers.blocks.BlockFTLDrive
 import de.wulkanat.www.new_frontiers.abstract_helpers.NFBlock
 import de.wulkanat.www.new_frontiers.blocks.BlockSpaceTeleporter
 
-enum class Blocks(value: NFBlock) {
+enum class Blocks(val value: NFBlock) {
     SPACE_TELEPORTER(BlockSpaceTeleporter()),
     FTL_DRIVE(BlockFTLDrive());
-
-    val value: NFBlock by lazy {
-        value
-    }
-}
-
-class B(val a: String) {
-    init {
-        println("B Created")
-    }
-}
-
-class A(a: B) {
-    val b by lazy {
-        a
-    }
-}
-
-fun main() {
-    val a = A(B("something"))
-
-    println(a.b)
 }
